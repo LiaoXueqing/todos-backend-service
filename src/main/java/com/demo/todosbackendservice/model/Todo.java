ba10f0b3c967c42fe54123513d04702bd5a75609
Liao Xueqing <xueqingliao@foxmail.com>
2019-06-03 15:54:16 +0800

    Remove extra code

--- src/main/java/com/demo/todosbackendservice/model/Todo.java
+++ src/main/java/com/demo/todosbackendservice/model/Todo.java
@@ -26,28 +26,4 @@ public class Todo {
 
     private String content;
     private boolean isChecked;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public boolean isChecked() {
-        return isChecked;
-    }
-
-    public void setChecked(boolean checked) {
-        isChecked = checked;
-    }
 }
