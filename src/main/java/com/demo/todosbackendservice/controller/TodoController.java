019b390ca3ecd20b2e3531be4a7622c02d9c8d4a
Liao Xueqing <xueqingliao@foxmail.com>
2019-06-05 14:55:52 +0800

    设置/todos允许跨域访问，以方便前台服务调用

--- src/main/java/com/demo/todosbackendservice/controller/TodoController.java
+++ src/main/java/com/demo/todosbackendservice/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.demo.todosbackendservice.controller;
 import com.demo.todosbackendservice.model.Todo;
 import com.demo.todosbackendservice.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
